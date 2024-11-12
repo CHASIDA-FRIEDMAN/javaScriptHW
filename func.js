7f37745a1947743f11871a49c006920b7fb1d6cb
CHASIDA <chasida067@gmail.com>
2024-11-12 19:18:38 +0200

    four

--- func.js
+++ func.js
@@ -25,5 +25,27 @@ function f2() {
 }
 
 function f3(){
+    document.querySelector("#result3").innerHTML = ""
+    let year1 = document.querySelector("#year1").value
+    let year2 = document.querySelector("#year2").value
+    const ob3 = catalog.find(item=>item.year>year1&&item.year<year2)
+    document.querySelector("#result3").innerHTML = ob3.title
+}
+
+function f5(){
+    let newdisk={
+        title:document.getElementById("title").value,
+        artist:document.getElementById("artist").value,
+        company:document.getElementById("company").value,
+        country:document.getElementById("country").value,
+        price:document.getElementById("price").value,
+        year:document.getElementById("year").value,
+        id: (catalog.length+100)
+    }
+    catalog.push(newdisk)
+    alert("new disk added")
+}
+
+function f6(){
     
 }
